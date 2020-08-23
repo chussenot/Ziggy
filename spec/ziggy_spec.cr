1144411f3647c9ba813e493a82cc9c9b01e5f873
Clement Hussenot <clement.hussenot@manomano.com>
2020-08-23 22:20:42 +0200

    Add a listen method

--- spec/ziggy_spec.cr
+++ spec/ziggy_spec.cr
@@ -3,10 +3,16 @@ require "./spec_helper"
 describe Ziggy do
   it "works" do
     false.should eq(false)
-    server = Ziggy::Server.new
+    server = Ziggy::Server.new "localhost", 1234
     client = UDPSocket.new
     client.connect "localhost", 1234
-    client.puts "message"
+    client.puts "foo"
+    client.puts "foo2"
+
+    server.listen do |addr,msg,bytes|
+      puts "\nMessage from: #{addr}\n"
+      puts "\nMessage: #{msg}\n"
+    end
     server.close
   end
 end
--- src/ziggy.cr
+++ src/ziggy.cr
@@ -2,16 +2,19 @@ require "socket"
 
 module Ziggy
   class Server
-    # Create server
-    def initialize
+    @server : UDPSocket
+
+    def initialize( addr : String, port : Number )
       @server = UDPSocket.new
-      @server.bind "localhost", 1234
-      loop do
-        begin
-          puts @server.gets
-        rescue ex
-          STDERR.puts(ex)
-        end
+      @server.bind addr, port
+      @buffer = uninitialized UInt8[2048]
+    end
+
+    def listen (&result)
+      while (1==1)
+        bytes,addr  = @server.receive @buffer.to_slice
+        msg = String.new(@buffer.to_slice[0,bytes])
+        yield addr,msg,bytes
       end
     end
 
